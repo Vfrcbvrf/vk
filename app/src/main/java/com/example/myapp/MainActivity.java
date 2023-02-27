438ad7fbf6cea6d0bb4af241c0c6b92eab5d17e1
Maksim <agarkov05@inbox.ru>
2023-02-27 11:13:52 +0400

    123

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -3,27 +3,47 @@ package com.example.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+import jp.wasabeef.blurry.Blurry;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    ImageButton micro;
-    ImageButton camera;
-    ImageButton exit;
+    ImageButton micro, camera, exit;
+
+    ImageButton sms, people, tabs;
+
+    ImageView img1, img2, phon1, phon2, blur1, blur2;
+
+    TextView t1, t2;
+
 
     boolean micrOn = true;
     boolean camer = true;
+    boolean pages = true;
 
     final int DIALOG_EXIT = 1;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
@@ -32,23 +52,65 @@ public class MainActivity extends AppCompatActivity {
         micro= (ImageButton)findViewById(R.id.micro);
         camera = (ImageButton)findViewById(R.id.camera);
         exit = (ImageButton)findViewById(R.id.exit);
-
+        sms = (ImageButton)findViewById(R.id.sms);
+        people = (ImageButton)findViewById(R.id.people);
+        tabs = (ImageButton)findViewById(R.id.tabs);
+        img1 = (ImageView)findViewById(R.id.ava1);
+        img2 = (ImageView)findViewById(R.id.ava2);
+        phon1 = (ImageView)findViewById(R.id.phon1);
+        phon2 = (ImageView)findViewById(R.id.phon2);
+        t1 = (TextView)findViewById(R.id.textView);
+        t2 = (TextView)findViewById(R.id.textView2);
 
 
 
 
     }
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(hasFocus){
+            Blurry.with(this).sampling(10).capture(findViewById(R.id.phon1)).into(phon1);
+            Blurry.with(this).sampling(10).capture(findViewById(R.id.phon2)).into(phon2);
+
+        }
+
+    }
+
+
+
 
 
     public void mcr(View v) { // кнопка микрофона
-        if (!micrOn) micro.setImageResource(R.drawable.ic_baseline_mic_24);
-        else micro.setImageResource(R.drawable.ic_baseline_mic_off_24);
+        if (!micrOn){
+            micro.setImageResource(R.drawable.ic_baseline_mic_24);
+            micro.setBackgroundResource(R.drawable.circlebutton);
+
+            if (pages) t1.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_mic_24__, 0);
+            else t2.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_mic_24__, 0);
+
+        }
+        else {
+            micro.setImageResource(R.drawable.ic_baseline_mic_off_24);
+            micro.setBackgroundResource(R.drawable.blackcircle);
+
+            if (pages) t1.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_mic_off_24, 0);
+            else t2.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_mic_off_24, 0);
+        }
+
+
         micrOn = !micrOn;
     }
 
     public void cmr(View v) { // кнопка камеры
-        if (!camer) camera.setImageResource(R.drawable.ic_baseline_videocam_24);
-        else camera.setImageResource(R.drawable.ic_baseline_videocam_off_24);
+        if (!camer){
+            camera.setImageResource(R.drawable.ic_baseline_videocam_24);
+            camera.setBackgroundResource(R.drawable.circlebutton);
+
+        }
+        else{
+            camera.setImageResource(R.drawable.ic_baseline_videocam_off_24);
+            camera.setBackgroundResource(R.drawable.blackcircle);
+        }
         camer = !camer;
     }
 
@@ -59,6 +121,66 @@ public class MainActivity extends AppCompatActivity {
     public void finish(View v) { // кнопка выхода
         finish();
     }
+
+
+    @SuppressLint("IntentReset")
+    public void mms(View v){ // открытие приложения смс
+
+        Intent smsIntent = new Intent(Intent.ACTION_SENDTO);
+        smsIntent.addCategory(Intent.CATEGORY_DEFAULT);
+        smsIntent.setType("vnd.android-dir/mms-sms");
+        smsIntent.setData(Uri.parse("sms:"));
+        startActivity(smsIntent);
+
+    }
+
+
+    public void peop(View v) {
+
+        Intent pepoIntent = new Intent(Intent.ACTION_DEFAULT, ContactsContract.Contacts.CONTENT_URI);
+        startActivity(pepoIntent);
+    }
+
+    public void tab(View v){
+        if (!pages){
+            img1.setImageResource(R.drawable.photo1);
+            img2.setImageResource(R.drawable.photo2);
+
+            phon1.setImageResource(R.drawable.me);
+            phon2.setImageResource(R.drawable.layer);
+            Blurry.with(this).sampling(10).capture(findViewById(R.id.phon1)).into(phon1);
+            Blurry.with(this).sampling(10).capture(findViewById(R.id.phon2)).into(phon2);
+
+
+            t2.setText(R.string.someone);
+            t1.setText(R.string.you);
+
+
+            if (micrOn) t1.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_mic_24__, 0);
+            else t1.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_mic_off_24, 0);
+            t2.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_mic_off_24, 0);
+        }
+        else {
+            img1.setImageResource(R.drawable.photo2);
+            img2.setImageResource(R.drawable.photo1);
+
+
+            phon2.setImageResource(R.drawable.me);
+            phon1.setImageResource(R.drawable.layer);
+
+            Blurry.with(this).sampling(10).capture(findViewById(R.id.phon2)).into(phon2);
+            Blurry.with(this).sampling(10).capture(findViewById(R.id.phon1)).into(phon1);
+
+            t2.setText(R.string.you);
+            t1.setText(R.string.someone);
+
+            if (micrOn) t2.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_mic_24__, 0);
+            else t2.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_mic_off_24, 0);
+            t1.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_mic_off_24, 0);
+        }
+        pages = !pages;
+    }
+
 
     protected Dialog onCreateDialog(int id) {
         if (id == DIALOG_EXIT) {
@@ -93,6 +215,7 @@ public class MainActivity extends AppCompatActivity {
     void ve() {
         Toast.makeText(this, R.string.plus, Toast.LENGTH_SHORT).show();
     }
+
 
 
 
